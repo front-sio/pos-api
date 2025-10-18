2f4bd0367bbb1310a697b25d6a3e8561b4346494
leoproducts <masanja.developer@gmail.com>
2025-10-18 14:32:24 +0300

    update db.ts

--- auth-service/src/db/db.ts
+++ auth-service/src/db/db.ts
@@ -7,7 +7,10 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  
 });
+
+console.log(process.env.DATABASE_URL);
 
 export const db = drizzle(pool, { schema });
 
